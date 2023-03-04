4564f87ea3f5afc09b7611676b22a2529e5b4b35
Admin <rbrignas23@gmail.com>
2023-03-04 18:05:12 +0800

    List View Activity 03-04-23

---
+++ app/src/main/java/com/example/myapplication/my_list.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+
+class my_list : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_list)
+
+
+    }
+}
