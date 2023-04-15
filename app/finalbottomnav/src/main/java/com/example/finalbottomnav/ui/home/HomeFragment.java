4116550dd4bd24456fb40b3fb9cfba7d2e804f80
Admin <rbrignas23@gmail.com>
2023-04-15 17:09:24 +0800

    Final Bottom Navigation - April 15, 2023

---
+++ app/finalbottomnav/src/main/java/com/example/finalbottomnav/ui/home/HomeFragment.java
@@ -0,0 +1,47 @@
+package com.example.finalbottomnav.ui.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.finalbottomnav.SecondActivity;
+import com.example.finalbottomnav.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final Button buttonGoToSecond = binding.buttonGoToSecond;
+
+        buttonGoToSecond.setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), SecondActivity.class);
+            intent.putExtra("PASS_ME", "Hi I'm from Main Activity");
+            startActivity(intent);
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
